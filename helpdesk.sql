40a6cff8bf5bf27fd002c3e09ddd93ade72621e6
Agus Cahyadi <agusc.dev02@gmail.com>
2020-08-13 21:51:33 +0700

    sql

---
+++ helpdesk.sql
@@ -0,0 +1,73 @@
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Versi server:                 5.7.24 - MySQL Community Server (GPL)
+-- OS Server:                    Win64
+-- HeidiSQL Versi:               10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Membuang struktur basisdata untuk helpdesk
+CREATE DATABASE IF NOT EXISTS `helpdesk` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `helpdesk`;
+
+-- membuang struktur untuk table helpdesk.tb_issue
+CREATE TABLE IF NOT EXISTS `tb_issue` (
+  `id` int(11) NOT NULL,
+  `no_issue` varchar(10) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `nama_aplikasi` varchar(50) NOT NULL,
+  `issue` varchar(50) NOT NULL,
+  `keterangan` varchar(255) NOT NULL,
+  `id_programmer` int(11) NOT NULL DEFAULT '0',
+  `status` varchar(225) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table helpdesk.tb_perbaikan
+CREATE TABLE IF NOT EXISTS `tb_perbaikan` (
+  `id` int(11) NOT NULL,
+  `no_perbaikan` varchar(10) NOT NULL,
+  `nama_aplikasi` varchar(50) NOT NULL,
+  `id_issue` int(3) NOT NULL,
+  `id_programmer` int(11) NOT NULL,
+  `perbaikan` varchar(50) NOT NULL,
+  `keterangan` varchar(225) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_issue` (`id_issue`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Pengeluaran data tidak dipilih.
+
+-- membuang struktur untuk table helpdesk.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(225) NOT NULL,
+  `level` varchar(225) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Pengeluaran data tidak dipilih.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
